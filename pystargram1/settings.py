5ae86ac7295dc48d20604e90174cc466fc351481
parkjisu <qkrwltn28@gmail.com>
2019-03-07 19:29:52 +0900

    second

--- pystargram1/settings.py
+++ pystargram1/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,3 +118,7 @@ MEDIA_ROOT = BASE_DIR
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
